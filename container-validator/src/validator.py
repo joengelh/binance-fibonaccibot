f200b473891d196f9f7782ad65fcf5baa0d00282
joengelh <73387330+joengelh@users.noreply.github.com>
2021-04-23 00:40:14 +0200

    automated commit for update saving

--- container-validator/src/validator.py
+++ container-validator/src/validator.py
@@ -13,11 +13,11 @@ with open('fibLvl.json') as file:
 
 def calculateResult(resultData, bA, i):
     stopId = resultData[resultData[0] == resultData[0].min()]
-    percentChange = ((float(stopId[1]) - float(bA[4][i])) / float(bA[4][i])) * 100
+    percentChange = ((stopId[1] - bA[4][i]) / bA[4][i]) * 100
     #update database to include buy statement
     sql = ("UPDATE table001 SET" +
         " resultpercent = '" + str(percentChange) +
-        "', stopid = '" + str(int(stopId[0])) +
+        "', stopid = '" + str(stopId[0]) +
         "' WHERE id = '" + str(bA[0][i]) +
         "';")
     print("updated db to include results")
@@ -27,27 +27,31 @@ def runValidation():
     global timescale
     timescale = timescaledbAccess.timescaleAccess()
     sql = ("SELECT id, symbol, takeprofit, stoploss, askprice" +
-          " FROM table001 WHERE" + 
-          " resultpercent IS NULL " +
-          " AND takeprofit IS NOT NULL;")
+        " FROM table001 WHERE" +
+        " resultpercent IS NULL " +
+        " AND takeprofit IS NOT NULL;")
     bA = pd.DataFrame(timescale.sqlQuery(sql))
+    bA[0] = pd.to_numeric(bA[0])
+    bA[2] = pd.to_numeric(bA[2])
+    bA[3] = pd.to_numeric(bA[3])
+    bA[4] = pd.to_numeric(bA[4])
     if len(bA) > 0:
         for i, row in bA.iterrows():
-            #get point of sl and tp
-            sql = ("select id, askprice from table001" + " where id > '" + str(int(bA[0][i])) + 
-            "' and symbol = '" + str(bA[1][i]) + "';")
+            sql = ("select id, askprice from table001" + " where id > '" + str(bA[0][i]) +
+                    "' and symbol = '" + str(bA[1][i]) + "';")
             validated = pd.DataFrame(timescale.sqlQuery(sql))
-            tpData = validated[validated[1] >= float(bA[2][i])]
-            slData = validated[validated[1] <= float(bA[3][i])]
-            #check if sl data and tp data have been met
+            validated[0] = pd.to_numeric(validated[0])
+            validated[1] = pd.to_numeric(validated[1])
+            tpData = validated[validated[1] >= bA[2][i]]
+            slData = validated[validated[1] <= bA[3][i]]
             if (len(slData) > 0 and len(slData) > 0 and
                 slData[0].min() < tpData[0].min()):
-                    print("executed 1")
-                    calculateResult(slData, bA, i)
+                print("executed 1")
+                calculateResult(slData, bA, i)
             elif (len(slData) > 0 and len(slData) > 0 and
-                slData[0].min() > tpData[0].min()): 
-                    print("executed 2")
-                    calculateResult(tpData, bA, i)
+                slData[0].min() > tpData[0].min()):
+                print("executed 2")
+                calculateResult(tpData, bA, i)
             elif len(slData) > 0:
                 print("executed 3")
                 calculateResult(slData, bA, i)
