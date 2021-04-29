891fa36d391f98310903d9a8badeb82f13e797fd
joengelh <73387330+joengelh@users.noreply.github.com>
2021-04-29 21:28:13 +0200

    move livetrading logic to class from crawler

--- tools/plotValidator.R
+++ tools/plotValidator.R
@@ -22,7 +22,8 @@ fR$northLevel <- NA
 #get list of buy advice instances
 sqlQuery <- dbSendQuery(con, "SELECT * FROM table001 
                      WHERE resultpercent IS NOT NULL
-                        and fiblevel >= 1;")
+                        and corvalue > '0.33'
+                        and fiblevel >= '1';")
 validated <- dbFetch(sqlQuery)
 dbClearResult(sqlQuery)
 
