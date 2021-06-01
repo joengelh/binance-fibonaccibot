7d8bcb8cdb6af94af45a8e3d010c77fad84cc7ed
joengelh <73387330+joengelh@users.noreply.github.com>
2021-06-01 12:48:00 +0200

    add round() to webui display

--- container-webui/src/public/sumResult.js
+++ container-webui/src/public/sumResult.js
@@ -1,6 +1,6 @@
 async function getHistory() {
 	const meanResultResponse = await fetch('/sumResult');
 	const meanResultData = await meanResultResponse.json();
-	document.getElementById("sum").innerHTML = parseFloat(meanResultData).toPrecision(4);
+	document.getElementById("sum").innerHTML = parseFloat(meanResultData).toPrecision(4) + " %";
 };
 getHistory();
