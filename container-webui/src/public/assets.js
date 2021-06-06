c1d1cc1f95c72ad95b83799e7f6a6bb38f159495
joengelh <73387330+joengelh@users.noreply.github.com>
2021-06-06 11:19:38 +0200

    add self-updating functionality to webui frontend

--- container-webui/src/public/assets.js
+++ container-webui/src/public/assets.js
@@ -2,5 +2,6 @@ async function getAssets() {
 	const assetsResponse = await fetch('/assets');
 	const assetsData = await assetsResponse.json();
 	document.getElementById("amount").innerHTML = parseFloat(assetsData).toPrecision(3) + " BNB";
+	setTimeout( getHistory, 5000 );
 };
 getAssets();
--- container-webui/src/public/recentSumResult.js
+++ container-webui/src/public/recentSumResult.js
@@ -2,5 +2,6 @@ async function getHistory() {
 	const resultResponse = await fetch('/recentSumResult');
 	const resultData = await resultResponse.json();
 	document.getElementById("recentSumResult").innerHTML = parseFloat(resultData).toPrecision(3) + " %";
+	setTimeout( getHistory, 5000 );
 };
 getHistory();
