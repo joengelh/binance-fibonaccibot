b888900ea403dda15040ba573135441a4bae1fc5
joengelh <73387330+joengelh@users.noreply.github.com>
2021-11-14 17:41:10 +0100

    intermedia save

--- container-predictor/src/main.rs
+++ container-predictor/src/main.rs
@@ -1,49 +1,52 @@
-#[macro_use]
-extern crate dotenv_codegen;
-
-extern crate dotenv;
-//extern crate redis;
-extern crate postgres;
-
-use postgres::{Connection, TlsMode};
-use postgres::tls::native_tls::NativeTls;
 use dotenv::dotenv;
 use std::env;
+use postgres::{Client, NoTls, Error};
 
-fn main() {
+fn main() -> Result<(), Error> {
+
     dotenv().ok();
-
-    let r_port: String = env::var("dbPort").unwrap();
-    let r_host: String = env::var("brokerFees").unwrap();
-    println!("Hello, {}", [r_host, r_port].join(" "));
-
-    let RedisVars = RedisVars {
-        db_port: env::var("rPort").unwrap(),
-        db_host: env::var("dbHost").unwrap(),
-        db_name: env::var("rName").unwrap(),
-        db_pw: env::var("POSTGRES_PASSWORD").unwrap(),
-    };
-
-    println!("{}", RedisVars.db_port);
 
     let postgres_path: String = [
         "postgresql://",
-        env::var("dbUser").unwrap(),
+        &env::var("dbUser").unwrap(),
         ":",
-        env::var("POSTGRES_PASSWORD").unwrap(),
+        &env::var("POSTGRES_PASSWORD").unwrap(),
         "@",
-        env::var("dbHost").unwrap(),
+        &env::var("dbHost").unwrap(),
         ":",
-        env::var("dbPort").unwrap(),
+        &env::var("dbPort").unwrap(),
         "/",
-        env::var("dbName").unwrap(),
+        &env::var("dbName").unwrap(),
     ].join("");
-    let mut conn = Client::connect(postgres_path).unwrap();
+
+    let mut client = Client::connect(&postgres_path, NoTls)?;
+    
+    client.query("SELECT id, name, country FROM author", &[])? {
+        let author = Author {
+            _id: row.get(0),
+            name: row.get(1),
+            country: row.get(2),
+        };
+        println!("Author {} is from {}", author.name, author.country);
+
+    Ok(())
 }
 
-struct RedisVars {
-    db_port: String,
-    db_host: String,
-    db_name: String,
-    db_pw: String, 
-}
+// keys to be cached
+// simulatedAvg
+//  value=str(round(sum(resultPercent)/len(resultPercent), 2)) + " %"
+// simulatedSum
+//  value=str(round(sum(resultPercent), 2)) + " %"
+// simulatedWinner
+//  value=round(sum(i > 0 for i in resultPercent), 2)
+// simulatedLoser
+//  value=round(sum(i < 0 for i in resultPercent), 2)
+//  for index, row in openTrades.iterrows():
+//  resultPercent.append(((float(stopId[0][0]) - float(row[1])) / float(row[1])) * 100 - self.brokerFees * 2)
+//  str(round(sum(resultPercent)/len(resultPercent), 2)) + " %"
+// assets
+// openTrades
+// sumResult
+//  select sum((resultpercent/100) * positioncost) from ' + 
+// recentSumResult
+//  select sum((resultpercent/100) * positioncost) from ' + 
