6ad4b16494a4b1854742ab5017a989a44cd75646
joengelh <73387330+joengelh@users.noreply.github.com>
2021-11-14 16:11:31 +0100

    intermedia save

--- container-predictor/src/main.rs
+++ container-predictor/src/main.rs
@@ -2,24 +2,48 @@
 extern crate dotenv_codegen;
 
 extern crate dotenv;
-extern crate redis;
+//extern crate redis;
+extern crate postgres;
 
+use postgres::{Connection, TlsMode};
+use postgres::tls::native_tls::NativeTls;
 use dotenv::dotenv;
 use std::env;
 
 fn main() {
-    let somestring = env::var("CARGO_HOME").unwrap();
-    let someotherstring = env::var("brokerFees").unwrap();
-    println!("Hello, {}", [somestring, someotherstring].join(""));
+    dotenv().ok();
 
+    let r_port: String = env::var("dbPort").unwrap();
+    let r_host: String = env::var("brokerFees").unwrap();
+    println!("Hello, {}", [r_host, r_port].join(" "));
+
+    let RedisVars = RedisVars {
+        db_port: env::var("rPort").unwrap(),
+        db_host: env::var("dbHost").unwrap(),
+        db_name: env::var("rName").unwrap(),
+        db_pw: env::var("POSTGRES_PASSWORD").unwrap(),
+    };
+
+    println!("{}", RedisVars.db_port);
+
+    let postgres_path: String = [
+        "postgresql://",
+        env::var("dbUser").unwrap(),
+        ":",
+        env::var("POSTGRES_PASSWORD").unwrap(),
+        "@",
+        env::var("dbHost").unwrap(),
+        ":",
+        env::var("dbPort").unwrap(),
+        "/",
+        env::var("dbName").unwrap(),
+    ].join("");
+    let mut conn = Client::connect(postgres_path).unwrap();
 }
 
-
-fn write_to_redis() -> redis::RedisResult<()> {
-    let client = redis::Client::open("redis://127.0.0.1/")?;
-    let mut con = client.get_connection()?;
-
-    /* do something here */
-
-    Ok(())
+struct RedisVars {
+    db_port: String,
+    db_host: String,
+    db_name: String,
+    db_pw: String, 
 }
