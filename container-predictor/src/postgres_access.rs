7c57ad9bd28531adb71492eb768bea577b1a98f2
joengelh <73387330+joengelh@users.noreply.github.com>
2021-11-15 20:01:50 +0100

    automated commit for update saving

--- container-predictor/src/postgres_access.rs
+++ container-predictor/src/postgres_access.rs
@@ -2,6 +2,10 @@ use dotenv::dotenv;
 use std::env;
 use postgres::{Client, NoTls, Error};
 
+struct Author {
+    id: i32
+}
+
 pub fn get_query_single() {
 
     dotenv().ok();
@@ -20,4 +24,13 @@ pub fn get_query_single() {
     ].join("");
 
     let mut client = Client::connect(&postgres_path, NoTls);
+
+    for row in client.query("SELECT id FROM table001;", &[])? {
+        let author = Author {
+            id: row.get(0),
+        };
+        println!("Author {} is from {}", author.name, author.country);
+    }
+
+    Ok(())
 }
