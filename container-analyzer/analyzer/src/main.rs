b12848eee2e5bae917f2b1b265ee8bc3dda92733
joengelh <73387330+joengelh@users.noreply.github.com>
2021-11-16 18:51:02 +0100

    add variable table to analyzer

--- container-analyzer/analyzer/src/main.rs
+++ container-analyzer/analyzer/src/main.rs
@@ -2,9 +2,13 @@ use binance_access;
 use postgres_access;
 use redis_access;
 
+use dotenv::dotenv;
+use std::env;
+
 use std::{thread, time};
 
 fn main() {
+    dotenv().ok();
     loop {
         cache_balance();
         cache_open_trades();
@@ -20,21 +24,24 @@ fn cache_balance() {
 }
 
 fn cache_open_trades() {
-    let sql = "SELECT count(*) FROM table001
-    where takeprofit is not null and resultpercent is null;";
+    let sql = ["SELECT count(*) FROM", 
+        &env::var("dbTable").unwrap_or_default(), 
+        "where takeprofit is not null and resultpercent is null;"].join(" ");
     let open_trades = postgres_access::get_count(&sql);
     redis_access::set_key_value("openTrades", 
         &open_trades.unwrap().to_string());
 }
 
 fn cache_sum_result() {
-    let sql = "SELECT count(resultpercent) FROM table001;";
+    let sql = ["SELECT count(resultpercent) FROM ",
+        &env::var("dbTable").unwrap_or_default()].join(" ");
     let closed_trades = postgres_access::get_count(&sql);
     let cutoff: i64 = 1;
     if &closed_trades.as_ref().unwrap() <= &&cutoff {
         redis_access::set_key_value("sumResult", "0 %");
     } else {
-        let sql = "select sum(resultpercent) from table001;";
+        let sql = ["select sum(resultpercent) from ",
+            &env::var("dbTable").unwrap_or_default()].join(" ");
         let sum_result = postgres_access::get_sum(&sql);
         redis_access::set_key_value("sumResult", 
         &format!("{}{}", &sum_result.unwrap().to_string(), " %"));
@@ -42,15 +49,17 @@ fn cache_sum_result() {
 }
 
 fn cache_recent_sum_result() {
-    let sql = "SELECT count(resultpercent) FROM table001
-    where time > now() - interval \'24 hours\';";
+    let sql = ["SELECT count(resultpercent) FROM",
+        &env::var("dbTable").unwrap_or_default(),
+        "where time > now() - interval \'24 hours\';"].join(" ");
     let closed_trades = postgres_access::get_count(&sql);
     let cutoff: i64 = 1;
     if &closed_trades.as_ref().unwrap() <= &&cutoff {
         redis_access::set_key_value("recentSumResult", "0 %");
     } else {
-        let sql = "select sum(resultpercent) from table001
-        where time > now() - interval \'24 hours\';";
+        let sql = ["select sum(resultpercent) from",
+            &env::var("dbTable").unwrap_or_default(),
+            "where time > now() - interval \'24 hours\';"].join(" ");
         let sum_result = postgres_access::get_sum(&sql);
         redis_access::set_key_value("recentSumResult", 
         &format!("{}{}", &sum_result.unwrap().to_string(), " %"));
