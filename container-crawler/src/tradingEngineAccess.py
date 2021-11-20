1d647ca1b3a08bb8c39e7212047d12c6973407c8
joengelh <73387330+joengelh@users.noreply.github.com>
2021-11-20 13:10:46 +0100

    automated commit for update saving

--- container-crawler/src/tradingEngineAccess.py
+++ container-crawler/src/tradingEngineAccess.py
@@ -96,8 +96,10 @@ class tradingAccess:
             #if no open trade for symbol exists and price in between 7th fiblvl
             for i in [7]:
                 if (int(self.postgres.sqlQuery(sql)[0][0]) == 0 and
-                    float(tick["priceChangePercent"]) <= -10 and
-                    statisticsTools["skew"] <= -0.1 and
+                    (float(tick["priceChangePercent"]) <= -10 and
+                    statisticsTools["skew"] <= -0.1) or 
+                    (float(tick["priceChangePercent"]) >= 5 and
+                    statisticsTools["skew"] >= 0.1) and
                     float(tick['askPrice']) < fibRetracement[3][i] and
                     float(tick['askPrice']) > fibRetracement[2][i]):
                     self.openTrade(fibRetracement, i, largeData, corValue, tick, statisticsTools)
