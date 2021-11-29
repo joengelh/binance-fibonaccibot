10a3207db50c15e168ce9661f3a811ec7458d852
joengelh <73387330+joengelh@users.noreply.github.com>
2021-11-29 16:26:25 +0100

    automated commit for update saving

--- container-predictor/src/predictionAccess.py
+++ container-predictor/src/predictionAccess.py
@@ -64,51 +64,77 @@ class predictAccess:
             port=6379, 
             db=0, 
             password=self.POSTGRES_PASSWORD)
-        #initiate resultpercent var
-        resultPercent = []
         #get unique simbol list
         sql = ("SELECT symbol, askprice FROM " + self.dbTable + 
             " where resultpercent is null and takeprofit is not null;")
         openTrades = pd.DataFrame(postgres.sqlQuery(sql))
         if len(openTrades) > 0:
             if self.liveTrading:
-        
-            #initiate empty output dataframe
-            for index, row in openTrades.iterrows():
-                #query for simulated stopid
-                sql = ("SELECT bidprice" +
-                    " FROM " + self.dbTable + " WHERE" +
-                    " symbol = '" + row[0] +
-                    "' ORDER BY id DESC LIMIT 1;")
-                stopId = pd.DataFrame(postgres.sqlQuery(sql))
-                resultPercent.append(((float(stopId[0][0]) - float(row[1])) / float(row[1])) * 100 - self.brokerFees * 2)
-            r.setex(
-                "simulatedAvg",
-                timedelta(minutes=15),
-                value=str(round(sum(resultPercent)/len(resultPercent), 2)) + " %"
-            )
-            if self.liveTrading:    
-                r.setex(
-                    "simulatedSum",
+                result = []
+                sql = ("SELECT symbol, askprice, positioncost FROM " + self.dbTable + 
+                    " where resultpercent is null and positioncost is not null;")
+                openTrades = pd.DataFrame(postgres.sqlQuery(sql))
+                if len(openTrades) > 0:
+                    for index, row in openTrades.iterrows():
+                        #query for simulated stopid
+                        sql = ("SELECT bidprice" +
+                            " FROM " + self.dbTable + " WHERE" +
+                            " symbol = '" + row[0] +
+                            "' ORDER BY id DESC LIMIT 1;")
+                        stopId = pd.DataFrame(postgres.sqlQuery(sql))
+                        result.append((((float(stopId[0][0]) - float(row[1])) / float(row[1])) 
+                            * 100 - self.brokerFees * 2) / 100 * float(row[2]))
+                    r.setex(
+                    "simulatedAvg",
                     timedelta(minutes=15),
-                    value=str(round(sum(resultPercent)/100 * self.liveVolume, 2)) + " " + self.baseCurrency
-                )
+                    value=str(round(sum(resultPercent)/len(resultPercent), 2)) + " %"
+                    )
+                    r.setex(
+                        "simulatedSum",
+                        timedelta(minutes=15),
+                        value=str(round(sum(result), 2)) + " " + self.baseCurrency
+                    )
+                    r.setex(
+                        "simulatedWinner",
+                        timedelta(minutes=15),
+                        value=round(sum(i > 0 for i in resultPercent), 2)
+                    )  
+                    r.setex(
+                        "simulatedLoser",
+                        timedelta(minutes=15),
+                        value=round(sum(i < 0 for i in resultPercent), 2)
+                    )
+
             else:
+                resultPercent = []
+                for index, row in openTrades.iterrows():
+                    #query for simulated stopid
+                    sql = ("SELECT bidprice" +
+                        " FROM " + self.dbTable + " WHERE" +
+                        " symbol = '" + row[0] +
+                        "' ORDER BY id DESC LIMIT 1;")
+                    stopId = pd.DataFrame(postgres.sqlQuery(sql))
+                    resultPercent.append(((float(stopId[0][0]) - float(row[1])) / float(row[1])) * 100 - self.brokerFees * 2)
+                r.setex(
+                    "simulatedAvg",
+                    timedelta(minutes=15),
+                    value=str(round(sum(resultPercent)/len(resultPercent), 2)) + " %"
+                )
                 r.setex(
                     "simulatedSum",
                     timedelta(minutes=15),
                     value=str(round(sum(resultPercent), 2)) + " %"
                 )
-            r.setex(
-                "simulatedWinner",
-                timedelta(minutes=15),
-                value=round(sum(i > 0 for i in resultPercent), 2)
-            )  
-            r.setex(
-                "simulatedLoser",
-                timedelta(minutes=15),
-                value=round(sum(i < 0 for i in resultPercent), 2)
-            )
+                r.setex(
+                    "simulatedWinner",
+                    timedelta(minutes=15),
+                    value=round(sum(i > 0 for i in resultPercent), 2)
+                )  
+                r.setex(
+                    "simulatedLoser",
+                    timedelta(minutes=15),
+                    value=round(sum(i < 0 for i in resultPercent), 2)
+                )
         else:
             self.reset(r)
         postgres.databaseClose()
